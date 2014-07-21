Jianwei-Wang/linux-drm-fsl-dcu

i915: split some DP modesetting code into a separate function
this is just prep work for mst support.

Signed-off-by: Dave Airlie <airlied@redhat.com>

drivers/gpu/drm/i915/intel_drv.h
@@ -683,6 +683,7 @@ void intel_ddi_fdi_disable(struct drm_crtc *crtc);
 void intel_ddi_get_config(struct intel_encoder *encoder,
 			  struct intel_crtc_config *pipe_config);
 
+void intel_ddi_mode_set_dp(struct intel_encoder *encoder);
 
 /* intel_display.c */
 const char *intel_output_name(int output);

Starrykernel/kernel_xiaomi_veux

Reviewed-by: Todd Previte <tprevite@gmail.com>

drivers/gpu/drm/i915/intel_drv.h
@@ -718,6 +718,7 @@ void intel_ddi_fdi_disable(struct drm_crtc *crtc);
 void intel_ddi_get_config(struct intel_encoder *encoder,
 			  struct intel_crtc_config *pipe_config);
 
+void intel_ddi_init_dp_buf_reg(struct intel_encoder *encoder);
 
 /* intel_display.c */
 const char *intel_output_name(int output);