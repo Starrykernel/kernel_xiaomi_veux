Starrykernel/kernel_xiaomi_veux

commit 3b33c84542f6feff31e72b20d1dc88b724d7af55

power: supply: bq25890_charger: Add debugging output of failed initialization
To ease adding a new part variant some debugging is handy.

Reviewed-by: Krzysztof Kozlowski <krzk@kernel.org>
Signed-off-by: Angus Ainslie (Purism) <angus@akkea.ca>
Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

drivers/power/supply/bq25890_charger.c
@@ -608,30 +608,40 @@ static int bq25890_hw_init(struct bq25890_device *bq)
 	};
 
 	ret = bq25890_chip_reset(bq);
-	if (ret < 0)
+	if (ret < 0) {
+		dev_dbg(bq->dev, "Reset failed %d\n", ret);
 		return ret;
+	};
 
 	/* disable watchdog */
 	ret = bq25890_field_write(bq, F_WD, 0);
-	if (ret < 0)
+	if (ret < 0) {
+		dev_dbg(bq->dev, "Disabling watchdog failed %d\n", ret);
 		return ret;
+	};
 
 	/* initialize currents/voltages and other parameters */
 	for (i = 0; i < ARRAY_SIZE(init_data); i++) {
 		ret = bq25890_field_write(bq, init_data[i].id,
 					  init_data[i].value);
-		if (ret < 0)
+		if (ret < 0) {
+			dev_dbg(bq->dev, "Writing init data failed %d\n", ret);
 			return ret;
+		};
 	}
 
 	/* Configure ADC for continuous conversions. This does not enable it. */
 	ret = bq25890_field_write(bq, F_CONV_RATE, 1);
-	if (ret < 0)
+	if (ret < 0) {
+		dev_dbg(bq->dev, "Config ADC failed %d\n", ret);
 		return ret;
+	};
 
 	ret = bq25890_get_chip_state(bq, &state);
-	if (ret < 0)
+	if (ret < 0) {
+		dev_dbg(bq->dev, "Get state failed %d\n", ret);
 		return ret;
+	};
 
 	mutex_lock(&bq->lock);
 	bq->state = state;
@@ -766,6 +776,9 @@ static int bq25890_fw_read_u32_props(struct bq25890_device *bq)
 		if (ret < 0) {
 			if (props[i].optional)
 				continue;
+
+			dev_err(bq->dev, "Unable to read property %d %s\n", ret,
+				props[i].name);
 
 			return ret;
 		}

commit 3f71956eb6a7fbf86194b6f7f24c4b6a52bb6f5e

power: supply: bq25890_charger: fix semicolon.cocci warnings
drivers/power/supply/bq25890_charger.c:614:2-3: Unneeded semicolon
drivers/power/supply/bq25890_charger.c:621:2-3: Unneeded semicolon
drivers/power/supply/bq25890_charger.c:630:3-4: Unneeded semicolon
drivers/power/supply/bq25890_charger.c:638:2-3: Unneeded semicolon
drivers/power/supply/bq25890_charger.c:644:2-3: Unneeded semicolon

 Remove unneeded semicolon.

Generated by: scripts/coccinelle/misc/semicolon.cocci

Fixes: 3b33c84542f6 ("power: supply: bq25890_charger: Add debugging output of failed initialization")
CC: Angus Ainslie (Purism) <angus@akkea.ca>
Signed-off-by: kbuild test robot <fengguang.wu@intel.com>
Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

drivers/power/supply/bq25890_charger.c
@@ -628,14 +628,14 @@ static int bq25890_hw_init(struct bq25890_device *bq)
 	if (ret < 0) {
 		dev_dbg(bq->dev, "Reset failed %d\n", ret);
 		return ret;
-	};
+	}
 
 	/* disable watchdog */
 	ret = bq25890_field_write(bq, F_WD, 0);
 	if (ret < 0) {
 		dev_dbg(bq->dev, "Disabling watchdog failed %d\n", ret);
 		return ret;
-	};
+	}
 
 	/* initialize currents/voltages and other parameters */
 	for (i = 0; i < ARRAY_SIZE(init_data); i++) {
@@ -644,7 +644,7 @@ static int bq25890_hw_init(struct bq25890_device *bq)
 		if (ret < 0) {
 			dev_dbg(bq->dev, "Writing init data failed %d\n", ret);
 			return ret;
-		};
+		}
 	}
 
 	/* Configure ADC for continuous conversions. This does not enable it. */
@@ -652,13 +652,13 @@ static int bq25890_hw_init(struct bq25890_device *bq)
 	if (ret < 0) {
 		dev_dbg(bq->dev, "Config ADC failed %d\n", ret);
 		return ret;
-	};
+	}
 
 	ret = bq25890_get_chip_state(bq, &state);
 	if (ret < 0) {
 		dev_dbg(bq->dev, "Get state failed %d\n", ret);
 		return ret;
-	};
+	}
 
 	mutex_lock(&bq->lock);
 	bq->state = state;