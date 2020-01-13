arter97/caf_msm-5.4

ASoC: core: Add compat ioctl support for listen driver
Change adds compat ioctl to listen driver for supporting 32 bit userspace
client when kernel is compiled for 64 bit.

Change-Id: Ie6a6fb1f2b6ea569e03b02bc520ef79b95c1009c
Signed-off-by: Gopikrishnaiah Anandan <agopik@codeaurora.org>
Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>

sound/core/pcm_compat.c
@@ -480,6 +480,20 @@ static int snd_compressed_ioctl32(struct snd_pcm_substream *substream,
 	pr_debug("%s called with cmd = %d\n", __func__, cmd);
 	return err;
 }
+static int snd_user_ioctl32(struct snd_pcm_substream *substream,
+			  unsigned int cmd, void __user *arg)
+{
+	struct snd_pcm_runtime *runtime;
+	int err = -ENOIOCTLCMD;
+
+	if (PCM_RUNTIME_CHECK(substream))
+		return -ENXIO;
+	runtime = substream->runtime;
+	if (substream->ops->compat_ioctl)
+		err = substream->ops->compat_ioctl(substream, cmd, arg);
+	return err;
+}
+
 
 static long snd_pcm_ioctl_compat(struct file *file, unsigned int cmd, unsigned long arg)
 {
@@ -551,6 +565,8 @@ static long snd_pcm_ioctl_compat(struct file *file, unsigned int cmd, unsigned l
 	default:
 		if (_IOC_TYPE(cmd) == 'C')
 			return snd_compressed_ioctl32(substream, cmd, argp);
+		else if (_IOC_TYPE(cmd) == 'U')
+			return snd_user_ioctl32(substream, cmd, argp);
 	}
 
 	return -ENOIOCTLCMD;

Starrykernel/kernel_xiaomi_veux

Signed-off-by: Meng Wang <mwang@codeaurora.org>

sound/core/pcm_compat.c
@@ -654,6 +654,15 @@ static int snd_compressed_ioctl32(struct snd_pcm_substream *substream,
 	pr_debug("%s called with cmd = %d\n", __func__, cmd);
 	return err;
 }
+
+static int snd_pcm_ioctl32_compat(struct snd_pcm_substream *substream,
+			unsigned int cmd, void __user *arg)
+{
+	if (_IOC_TYPE(cmd) == 'C' || _IOC_TYPE(cmd) == 'U')
+		return snd_compressed_ioctl32(substream, cmd, arg);
+
+	return 0;
+}
 #endif
 
 static long snd_pcm_ioctl_compat(struct file *file, unsigned int cmd, unsigned long arg)
@@ -735,8 +744,7 @@ static long snd_pcm_ioctl_compat(struct file *file, unsigned int cmd, unsigned l
 #endif /* CONFIG_X86_X32 */
 #ifdef CONFIG_AUDIO_QGKI
 	default:
-		if (_IOC_TYPE(cmd) == 'C')
-			return snd_compressed_ioctl32(substream, cmd, argp);
+		return snd_pcm_ioctl32_compat(substream, cmd, argp);
 #endif
 	}
 