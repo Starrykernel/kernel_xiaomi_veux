Starrykernel/kernel_xiaomi_veux

commit a0eebe795f237bd465bc0d160b220d31472bbd1b

block: disable multi-page bvec
The multi-page bvec is resulting in the performance regression on
UFS devices in sequential read test for upto ~9%. Hence, disable
it as a workaround.

Change-Id: I1dc3f5e710fd1c9a76f32e3144a0081df755ea26
Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

block/bio.c
@@ -806,9 +806,7 @@ bool __bio_try_merge_page(struct bio *bio, struct page *page,
 	if (bio->bi_vcnt > 0) {
 		struct bio_vec *bv = &bio->bi_io_vec[bio->bi_vcnt - 1];
 
-		if (page_is_mergeable(bv, page, len, off, same_page)) {
-			if (bio->bi_iter.bi_size > UINT_MAX - len)
-				return false;
+		if (page == bv->bv_page && off == bv->bv_offset + bv->bv_len) {
 			bv->bv_len += len;
 			bio->bi_iter.bi_size += len;
 			return true;

commit 5f1c75baec3149ae648ab466f12e03f78021c8c4

block: Fix the setting of same_page variable
The same_page variable inside the function __bio_try_merge_page()
must be set to true if the merge happened within the same page.
As some the callers of this function take action based on that.

Change-Id: Iae4a7170aeaffcc0b9c424368c7d8127d51a177b
Fixes: a0eebe795f23 ("block: disable multi-page bvec")
Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

block/bio.c
@@ -807,6 +807,7 @@ bool __bio_try_merge_page(struct bio *bio, struct page *page,
 		struct bio_vec *bv = &bio->bi_io_vec[bio->bi_vcnt - 1];
 
 		if (page == bv->bv_page && off == bv->bv_offset + bv->bv_len) {
+			*same_page = true;
 			bv->bv_len += len;
 			bio->bi_iter.bi_size += len;
 			return true;