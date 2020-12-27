11693fe7b5ad6590f7cf509e2b7c18095705293e
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-26 18:49:20 -0800

    techpack: camera: Stub out the camera_debug_util API and compile it out
    A measurably significant amount of CPU time is spent in these routines
    while the camera is open. These are also responsible for a grotesque
    amount of dmesg spam, so let's nuke them.
    
    [Kazuki: Port to v5.4.]
    
    Change-Id: I0ebc8eee6922f3a24ee4f8cd324f4d9535a62306
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- techpack/camera/drivers/cam_utils/cam_debug_util.h
+++ techpack/camera/drivers/cam_utils/cam_debug_util.h
@@ -111,8 +111,10 @@ static inline void cam_debug_log(unsigned int module_id, const char *func,
  * @fmt       :  Formatted string which needs to be print in the log
  *
  */
-void cam_debug_trace(unsigned int tag, unsigned int module_id,
-	const char *func, const int line, const char *fmt, ...);
+static inline void cam_debug_trace(unsigned int tag, unsigned int module_id,
+	const char *func, const int line, const char *fmt, ...)
+{
+}
 
 /*
  * cam_get_module_name()
@@ -121,7 +123,10 @@ void cam_debug_trace(unsigned int tag, unsigned int module_id,
  *
  * @module_id :  Module ID which is using this function
  */
-const char *cam_get_module_name(unsigned int module_id);
+static inline const char *cam_get_module_name(unsigned int module_id)
+{
+	return NULL;
+}
 
 /*
  * CAM_TRACE
@@ -251,7 +256,7 @@ static inline const struct camera_debug_settings *cam_debug_get_settings(void)
 static inline ssize_t cam_debug_sysfs_node_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
-	return count;
+	return 0;
 }
 
 #endif /* _CAM_DEBUG_UTIL_H_ */
