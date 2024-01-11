eqs-development/android_kernel_motorola_sm8475-modules

msm: ipa: page pool recycling enhancements
Added the following enhancements to page pool recycling logic.

1) Common page pool for coalescing/default pipe.
2) Common page pool will hold 5 times the rx_pool_sz (224) for page
   recycling pool and 3 times for temp pool. This is done to sustain
   50 stream scenarios.

Change-Id: I55adfffa101df33c46e95b0214fb7338644e8d7c
Signed-off-by: Chaitanya Pratapa <cpratapa@codeaurora.org>

qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -8055,6 +8055,10 @@ static int ipa3_pre_init(const struct ipa3_plat_drv_res *resource_p,
 	/* Initialize Page poll threshold. */
 	ipa3_ctx->page_poll_threshold = IPA_PAGE_POLL_DEFAULT_THRESHOLD;
 
+	/* Use common page pool for Def/Coal pipe. */
+	if (ipa3_ctx->ipa_hw_type >= IPA_HW_v5_1)
+		ipa3_ctx->wan_common_page_pool = true;
+
 	/* assume clock is on in virtual/emulation mode */
 	if (ipa3_ctx->ipa3_hw_mode == IPA_HW_MODE_VIRTUAL ||
 	    ipa3_ctx->ipa3_hw_mode == IPA_HW_MODE_EMULATION)

Starrykernel/kernel_xiaomi_veux

Signed-off-by: Ashok Vuyyuru <quic_avuyyuru@quicinc.com>

drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -7554,6 +7554,10 @@ static int ipa3_pre_init(const struct ipa3_plat_drv_res *resource_p,
 	/* Initialize Page poll threshold. */
 	ipa3_ctx->page_poll_threshold = IPA_PAGE_POLL_DEFAULT_THRESHOLD;
 
+	/* Use common page pool for Def/Coal pipe. */
+	if (ipa3_ctx->ipa_hw_type >= IPA_HW_v4_9)
+		ipa3_ctx->wan_common_page_pool = true;
+
 	/* assume clock is on in virtual/emulation mode */
 	if (ipa3_ctx->ipa3_hw_mode == IPA_HW_MODE_VIRTUAL ||
 	    ipa3_ctx->ipa3_hw_mode == IPA_HW_MODE_EMULATION)