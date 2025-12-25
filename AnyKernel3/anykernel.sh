95b275b044fda8ce89edffc4222fc765b7ab3205
Mr. Amrita Das <bhabanidas431@gmail.com>
2025-12-25 18:47:48 +0530

    Add AnyKernel3
    * From - https://github.com/Starrykernel/AnyKernel3.git
    * Also do some needed changes in the build script

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,51 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=Starry Kernel By Taki/Amrito
+do.devicecheck=1
+do.cleanup=1
+device.name1=veux
+device.name2=peux
+'; } # end properties
+
+
+### AnyKernel install
+# boot shell variables
+BLOCK=boot;
+IS_SLOT_DEVICE=auto;
+NO_BLOCK_DISPLAY=1;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+. tools/ak3-custom.sh;
+
+# boot install
+split_boot;
+
+flash_boot;
+## end boot install
+
+
+# vendor_boot shell variables
+BLOCK=vendor_boot;
+
+# reset for vendor_boot patching
+reset_ak;
+
+# vendor_boot install
+split_boot;
+
+check_cmdline;
+check_vendor_hals;
+
+flash_boot;
+## end vendor_boot install
+
+
+# dtbo install
+erase_dtbo;
+## end of dtbo install
+
