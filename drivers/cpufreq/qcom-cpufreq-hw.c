kdrag0n/proton_kernel_redbull

cpufreq: qcom-hw: Log reported voltage for each OPP
Required for calculating simplified energy models.

Signed-off-by: Danny Lin <danny@kdrag0n.dev>

drivers/cpufreq/qcom-cpufreq-hw.c
@@ -447,8 +447,8 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 
 		cur_freq = c->table[i].frequency;
 
-		dev_dbg(dev, "index=%d freq=%d, core_count %d\n",
-			i, c->table[i].frequency, core_count);
+		dev_info(dev, "index=%d freq=%d volt=%u, core_count %d\n",
+			 i, c->table[i].frequency, volt, core_count);
 
 		if (!of_find_freq(of_table, of_len, c->table[i].frequency)) {
 			c->table[i].frequency = CPUFREQ_ENTRY_INVALID;

Starrykernel/kernel_xiaomi_veux

Signed-off-by: RizkyMaulana58 <carlotta.montelli.0201@gmail.com>

drivers/cpufreq/qcom-cpufreq-hw.c
@@ -469,8 +469,8 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 			freq = cpu_hw_rate / 1000;
 
 		c->table[i].frequency = freq;
-		dev_dbg(dev, "index=%d freq=%d, core_count %d\n",
-				i, c->table[i].frequency, core_count);
+		dev_dbg(dev, "index=%d freq=%d volt=%u, core_count %d\n",
+				i, c->table[i].frequency, volt, core_count);
 
 		if (core_count != max_cores)
 			c->table[i].flags  = CPUFREQ_BOOST_FREQ;